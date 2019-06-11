463c8987e5ae81b2f159f848c526bd9cf0c67c68
theFay <fei.minhao1990@gmail.com>
2019-06-11 21:34:35 +0200

    replace api to coinegg

--- webapp/js/index.js
+++ webapp/js/index.js
@@ -110,6 +110,7 @@ $(function () {
   let currentTsp = rd(10, 30);
   $("#idxCurrent").html(currentTsp);
 
+  /**获取doraCMS 新闻内容 */
   $.ajax({
     url: "http://biut.io:8080/api/v0/content/getList",
     type: "GET",
@@ -121,20 +122,46 @@ $(function () {
     }
   });
 
+  /**获取sec btc的信息 */
   $.ajax({
-    url: 'https://api.coinegg.im/api/v1/kline/region/eth?coin=sec',
+    url: 'http://scan.biut.io/forwarding?url=https://trade.coinegg.com/web/symbol/ticker?right_coin=btc',
     type: 'GET',
     dataType: 'json',
     success: function (result) {
-      console.log(result);
-      let rate = (Number(result[0][1]) - Number(result[result.length-1][1])) / Number(result[0][1]);
-      $(".ethRiseFall").html((rate*100).toFixed(2));
-    },
-    error: function (err) {
-      console.log(err)
+      var btc_sec = result.data.filter(function (coin) {
+        return coin.symbol === 'sec_btc';
+      })
+      $('#btcPrice').html(btc_sec[0].price);
+      $('.btnRiseFall').html('+ ' + btc_sec[0].chg + '%');
+      if (Number(btc_sec[0].chg) < 0) {
+        $('.btnRiseFall').html('- ' + Math.abs(btc_sec[0].chg) + '%');
+      } else {
+        $('.btnRiseFall').html('+ ' + Math.abs(btc_sec[0].chg) + '%');
+      }
     }
   });
 
+  /**获取sec eth的信息 */
+  $.ajax({
+    url: 'http://scan.biut.io/forwarding?url=https://trade.coinegg.com/web/symbol/ticker?right_coin=eth',
+    type: 'GET',
+    dataType: 'json',
+    success: function (result) {
+      var eth_sec = result.data.filter(function (coin) {
+        return coin.symbol === 'sec_eth';
+      })
+      $('#ethPrice').html(eth_sec[0].price);
+      $('.ethRiseFall').html('+ ' + eth_sec[0].chg + '%');
+      if (Number(eth_sec[0].chg) < 0) {
+        $('.ethRiseFall').html('- ' + Math.abs(eth_sec[0].chg) + '%');
+      } else {
+        $('.ethRiseFall').html('+ ' + Math.abs(eth_sec[0].chg) + '%');
+      }
+      getEthPrice(eth_sec[0].price, eth_sec[0].chg);
+    }
+  });
+
+  /**获取节点信息 */
   $.ajax({
     url: 'http://scan.biut.io/nodeinfoapi',
     type: 'GET',
@@ -144,6 +171,7 @@ $(function () {
     }
   });
 
+  /**获取全网交易数量 */
   $.ajax({
     url: 'http://scan.biut.io/alltx',
     type: 'GET',
@@ -173,6 +201,7 @@ $(function () {
     }
   })
 
+  /**获取区块高度 */
   $.ajax({
     url: 'http://scan.biut.io/blockchain',
     type: 'GET',
@@ -181,33 +210,25 @@ $(function () {
       $("#idxHeight1").html(result.secblockchain.length - 1)
       $("#idxHeight2").html(result.senblockchain.length - 1)
     }
-  })
-
-  var webSocketClient = new WebSocket("wss://api.fcoin.com/v2/ws")
-  webSocketClient.onopen = function () {
-    webSocketClient.send(JSON.stringify({"cmd":"sub","args":["ticker.secbtc", "ticker.seceth", "ticker.sec"],"id":"sec_official"}))
-  }
-
-  webSocketClient.onmessage = function (evt) {
-    var data = JSON.parse(evt.data)
-    switch(data.type) {
-      case 'ticker.seceth':
-        $("#ethPrice").html(data.ticker[0])
-        $("#usdtPrice").html(data.ticker[3] / 1000)
-        break;
-      default:
-        break;
-    }
-  }
-
-  var ping = setTimeout(function () {
-    webSocketClient.send(JSON.stringify({"cmd": "ping", "args": [new Date().getTime()], "id": "sec_official"}))
-    ping = setTimeout(function () {
-      webSocketClient.send(JSON.stringify({"cmd": "ping", "args": [new Date().getTime()], "id": "sec_official"}))
-    }, 3500)
-  }, 35000)
-
+  });
 });
+
+function getEthPrice (price, chg) {
+  $.ajax({
+    url: 'https://market.coinegg.com/market/ticker?symbol=eth_usdt',
+    type: 'GET',
+    dataType: 'json',
+    success: function (result) {
+      var currentUSDT = Number(price) * Number(result.data[0].close);
+      $('#usdtPrice').html(currentUSDT.toFixed(2));
+      if (Number(chg) < 0) {
+        $('.useTRiseFall').html('- ' + Math.abs(chg) + '%');
+      } else {
+        $('.useTRiseFall').html('+ ' + Math.abs(chg) + '%');
+      }
+    }
+  })
+}
 
 //获取系统时间
 function systemTime () {
